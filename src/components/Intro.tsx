aa779f98df827b962297f615f1399796ce68c2be
abdelhak <abdelhak@groupado.com>
2022-02-05 18:57:06 +0100

    escape quotes in Intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,9 +5,9 @@ export default function Intro({ }: Props) {
     <div className="intro">
       <Container className="justify-between px-4 py-2 my-8">
         <p className="text-5xl text-red-500 font-bold">Hi!</p>
-        <h1 className="text-7xl text-red-500 font-bold">I'm Abdelhak Ajbouni</h1>
+        <h1 className="text-7xl text-red-500 font-bold">I&apos;m Abdelhak Ajbouni</h1>
         <p className="text-xl text-gray-500">
-          I'm a fullstack developer based in Tunis, Tunisia.
+          I&apos;m a fullstack developer based in Tunis, Tunisia.
         </p>
       </Container>
     </div >
