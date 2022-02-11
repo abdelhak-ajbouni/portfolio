e843e05ee5ae505ee7dddf4c18ce227e897ea161
abdelhak <abdelhak@groupado.com>
2022-02-11 22:34:39 +0100

    add TextArea

---
+++ src/components/common/TextArea.tsx
@@ -0,0 +1,18 @@
+import { toCamelCase } from 'src/libs/functions';
+
+export default function TextArea({ label, placeholder, register }: Props) {
+  return <textarea
+    className='shadow appearance-none border rounded w-full mb-2 p-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+    label={label}
+    placeholder={placeholder || label}
+    aria-label={label}
+    rows={5}
+    {...register(toCamelCase(label))}
+  />;
+}
+
+type Props = {
+  label: string;
+  placeholder?: string;
+  register: any;
+}
