e5aee402d15bd5d039e66c46ed6d65d3aac64189
abdelhak <abdelhak@groupado.com>
2022-02-19 10:55:00 +0100

    Updates

---
+++ _next/static/eux-LX-8Ovk0vYMmarXRZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d76cafc6-20d73ff1b75bdd9a.js","static/chunks/360-c336092ede2166e8.js","static/chunks/pages/index-a85e88c6c3f882ac.js"],"/_error":["static/chunks/pages/_error-fef04c19640916a1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
