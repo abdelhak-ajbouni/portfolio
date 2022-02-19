d7a62813fc796e5aa2106e045abaed2c2364efa2
abdelhak <abdelhak@groupado.com>
2022-02-19 15:38:14 +0100

    add imgix as an image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const { NODE_ENV, IMGIX_PATH } = process.env;
+const isDev = NODE_ENV === 'development'
+
+const images = isDev ? {} : {
+  loader: 'imgix',
+  path: IMGIX_PATH,
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images,
 }
 
 module.exports = nextConfig
