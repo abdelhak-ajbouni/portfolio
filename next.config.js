ee817aecff3261e99cbe16f9c9cfe8ee871dcf2d
abdelhak.ajbouni <abdelhak.ajbouni@gmail.com>
2022-05-14 23:35:23 +0100

    remove imgix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const { NODE_ENV, IMGIX_PATH } = process.env;
-const isDev = NODE_ENV === 'development'
-
-const images = isDev ? {} : {
-  loader: 'imgix',
-  path: IMGIX_PATH,
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images,
 }
 
 module.exports = nextConfig
