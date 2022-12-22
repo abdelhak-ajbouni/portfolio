1730e365c301f711164590aec9d8baa0ccebb3cf
Abdelhak <abdelhak.ajbouni@gmail.com>
2022-12-22 10:08:01 +0100

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,26 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import react from "@astrojs/react";
 import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://abdelhak-ajbouni.vercel.app',
+  integrations: [
+    tailwind(),
+    // TODO: fix skipped sitemap generation in build. error: No pages found! We can only detect sitemap routes for "static" builds
+    sitemap(),
+    react(),
+  ],
+  adapter: vercel(),
   vite: {
-    ssr: {
-      noExternal: ["svgo"],
-      external: ["svgo"]
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        external: ['astro-icon', 'react', 'react-dom'],
+      },
     }
   },
-  integrations: [tailwind(), sitemap(), image(), react()],
-  adapter: vercel()
 });
