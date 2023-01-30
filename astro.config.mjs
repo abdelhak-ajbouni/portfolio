2e051f44b901c22bdb5a50f3cb739e01baf0d2f5
Abdelhak <abdelhak.ajbouni@gmail.com>
2023-01-30 16:20:26 +0100

    improve images performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,22 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
+import image from "@astrojs/image";
+const url = 'https://abdelhak-ajbouni.com';
 
-const url = 'https://abdelhak-ajbouni.com'
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   site: url,
-  integrations: [
-    react(),
-    tailwind(),
-    partytown(),
-    sitemap({
-      customPages: [url],
-      lastmod: new Date("2022-12-26")
-    }),
-  ],
+  integrations: [react(), tailwind(), partytown(),
+  sitemap({
+    customPages: [url],
+    lastmod: new Date("2022-12-26")
+  }),
+  image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })],
   adapter: netlify()
 });
